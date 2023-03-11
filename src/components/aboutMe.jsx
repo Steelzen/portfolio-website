dccc87ce67af714e1f0999e17280268331a90a9a
Taehyung Kwon <katalogin88@gmail.com>
2023-03-11 22:30:39 +0000

    add technical skills section on aboutme

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -25,7 +25,44 @@ class AboutMe extends React.Component {
           <h1 className="aboutme-text"> About {this.state.details["name"]} </h1>
           <p className="aboutme-text">{this.state.details["summary"]}</p>
           <h4 className="aboutme-text">Technical Skills</h4>
-          <div classNmae="tech-img-container"></div>
+          <div className="tech-img-container">
+            <img
+              className="tech-img"
+              src="https://i.ibb.co/wN35QWj/Java-logo.png"
+            />
+            <img
+              className="tech-img"
+              src="https://i.ibb.co/SvdgbM6/Java-Script-logo.png"
+            />
+            <img
+              className="tech-img"
+              src="https://i.ibb.co/fSCwYVY/Python-logo.png"
+            />
+            <img
+              className="tech-img"
+              src="https://i.ibb.co/Xb5HCZ6/CSS-logo.png"
+            />
+            <img
+              className="tech-img"
+              src="https://i.ibb.co/Y3YGbr9/android-logo.jpg"
+            />
+            <img
+              className="tech-img"
+              src="https://i.ibb.co/L81KSpJ/React-logo.png"
+            />
+            <img
+              className="tech-img"
+              src="https://i.ibb.co/ydkptPJ/django-logo.jpg"
+            />
+            <img
+              className="tech-img"
+              src="https://i.ibb.co/9sVXgGz/SQL-logo.png"
+            />
+            <img
+              className="tech-img"
+              src="https://i.ibb.co/0rvrJfz/Git-logo.png"
+            />
+          </div>
           <h4 className="aboutme-text">Contact Me</h4>
           <div className="contactme-container">
             <a href={"mailto:" + this.state.details["email"]}>Email</a>
