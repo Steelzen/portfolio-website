ddf600fc90634734e0722d77d5a1633a4fea9374
Taehyung Kwon <katalogin88@gmail.com>
2023-03-12 21:05:52 +0000

    adjust project-text padding

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-// import "font-awesome/css/font-awesome.css";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
